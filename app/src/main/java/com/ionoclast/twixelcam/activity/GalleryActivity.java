6d24e922ce7931099504af7bae73b191a709f329
Brigham Toskin <brigham@ionoclast.com>
2016-02-15 23:35:57 -0800

    Recycle views, cleanup resources.

--- app/src/main/java/com/ionoclast/twixelcam/activity/GalleryActivity.java
+++ app/src/main/java/com/ionoclast/twixelcam/activity/GalleryActivity.java
@@ -9,8 +9,10 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.annotation.MainThread;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -19,6 +21,7 @@ import com.ionoclast.twixelcam.CameraTwixelator;
 import com.ionoclast.twixelcam.R;
 
 import java.io.File;
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Queue;
@@ -32,11 +35,15 @@ import java.util.concurrent.TimeUnit;
  */
 public class GalleryActivity extends Activity
 {
-	ViewPager mGalTwixelated;
-	File[] mFiles;
-	Bitmap[] mImages;
+	private static final String TAG = GalleryActivity.class.getSimpleName();
 
-	ThreadPoolExecutor mExecutor;
+	private ViewPager mGalTwixelated;
+	private File[] mFiles;
+	private Bitmap[] mImages;
+
+	private ThreadPoolExecutor mExecutor;
+
+	private boolean mStopping = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -44,9 +51,15 @@ public class GalleryActivity extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.gallery);
 
-		mGalTwixelated = (ViewPager)findViewById(R.id.lst_gallery);
+		mGalTwixelated = (ViewPager) findViewById(R.id.lst_gallery);
+	}
 
-		mFiles = listFiles();
+	@Override
+	public void onStart()
+	{
+		super.onStart();
+
+		mFiles = list_files();
 		mImages = new Bitmap[mFiles.length];
 		mExecutor = new ThreadPoolExecutor(1, 4, 1000, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(4));
 		mExecutor.execute(new LoadBitmapsTask(0, 4));
@@ -54,7 +67,32 @@ public class GalleryActivity extends Activity
 		mGalTwixelated.setAdapter(new TwixelatedPageAdapter());
 	}
 
-	private File[] listFiles()
+	@Override
+	protected void onStop()
+	{
+		mStopping = true;
+
+		mExecutor.shutdownNow();
+		mExecutor = null;
+
+		mGalTwixelated.setAdapter(null);
+
+		mFiles = null;
+		for(int i = 0; i < mImages.length; ++i)
+		{
+			if(mImages[i] != null)
+			{
+				mImages[i].recycle();
+			}
+		}
+		mImages = null;
+
+		mStopping = false;
+
+		super.onStop();
+	}
+
+	private File[] list_files()
 	{
 		File[] tFiles = CameraTwixelator.TWIXEL_DIR.listFiles();
 		Arrays.sort(tFiles, new Comparator<File>()
@@ -84,6 +122,11 @@ public class GalleryActivity extends Activity
 		{
 			for(int i = mStartIndex; i < mStartIndex + mCount; ++i)
 			{
+				if(Thread.interrupted())
+				{
+					return;
+				}
+
 				synchronized(mImages)
 				{
 					if(mImages[i] == null)
@@ -98,7 +141,7 @@ public class GalleryActivity extends Activity
 
 	private class TwixelatedPageAdapter extends PagerAdapter
 	{
-		private Queue<ImageView> mRecycledViews;
+		private Queue<ImageView> mRecycledViews = new ArrayDeque<ImageView>(4);
 
 		@Override
 		public int getCount()
@@ -109,7 +152,7 @@ public class GalleryActivity extends Activity
 		@Override
 		public Object instantiateItem(ViewGroup pParent, int pPosition)
 		{
-			ImageView tImageView = new ImageView(GalleryActivity.this);
+			ImageView tImageView = get_view();
 			tImageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
 
 			synchronized(mImages)
@@ -141,6 +184,11 @@ public class GalleryActivity extends Activity
 		@Override
 		public void destroyItem(ViewGroup pParent, final int pPosition, final Object pObject)
 		{
+			if(mStopping)
+			{
+				return;
+			}
+
 			// clean up bitmap
 			synchronized(mImages)
 			{
@@ -160,9 +208,23 @@ public class GalleryActivity extends Activity
 			}
 
 			// clear the image view
+			Log.d(TAG, "Recycling view, " + (mRecycledViews.size() + 1) + " in queue");
 			ImageView tImg = (ImageView)pObject;
 			tImg.setImageDrawable(null);
+			pParent.removeView(tImg);
+			mRecycledViews.add(tImg);
+		}
 
+		@MainThread
+		private ImageView get_view()
+		{
+			if(mRecycledViews.size() > 0)
+			{
+				Log.d(TAG, "Getting recycled view, " + (mRecycledViews.size() - 1) + " left in queue");
+				return mRecycledViews.remove();
+			}
+			Log.d(TAG, "Getting new view instance.");
+			return new ImageView(GalleryActivity.this);
 		}
 	}
 }
